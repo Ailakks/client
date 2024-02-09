05b001ba9cc67baafe75fd2d420fc034f7e53747
Ailakks <hola@cortmnzz.com>
2024-02-09 21:56:06 +0100

    feat: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import Layout from "./components/view/Layout";
 import Head from "./components/view/Head";
+import Header from "./components/view/Header";
 
 export default function App() {
   return (
-    <Layout head={<Head />} />
+    <Layout head={<Head />} header={<Header />} />
   )
 }
---
+++ src/components/view/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <div className="flex">
+            <input placeholder="Search" />
+            <button>Settings</button>
+        </div>
+    )
+}

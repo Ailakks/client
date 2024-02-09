1eb589993bf63a91538fdad642ae8d11d88e4986
Ailakks <hola@cortmnzz.com>
2024-02-10 00:56:12 +0100

    perf: layout divider style

--- src/components/view/Layout.tsx
+++ src/components/view/Layout.tsx
@@ -2,17 +2,17 @@ import React from "react";
 
 export default function Layout({ head, side, header, children }) {
     return (
-        <div className="flex divide-x divide-gray-300">
+        <div className="flex">
             <aside className="w-64 divide-y divide-gray-300">
                 <header className="flex items-center justify-center bg-gray-500 h-14 p-3">
                     {head}
                 </header>
-                <div className="p-3">
+                <div className="border-r border-gray-300 p-3">
                     {side}
                 </div>
             </aside>
             <div className="grow divide-y divide-gray-300">
-                <header className="flex items-center bg-gray-500 h-14 p-3">
+                <header className="flex items-center bg-gray-500 h-14 pr-3">
                     {header}
                 </header>
                 <main className="p-3">

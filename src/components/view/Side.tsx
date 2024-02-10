a7600143fc9ef71b5ad1782b658c33cb49b58988
Ailakks <hola@cortmnzz.com>
2024-02-10 01:40:45 +0100

    feat: side page list

--- src/components/view/Side.tsx
+++ src/components/view/Side.tsx
@@ -1,5 +1,34 @@
+const pages = [
+    {
+        id: 'home',
+        path: '/home',
+        name: 'Home',
+        icon: ''
+    },
+    {
+        id: 'recent',
+        path: '/recent',
+        name: 'Recent',
+        icon: ''
+    },
+    {
+        id: 'trash',
+        path: '/trash',
+        name: 'Trash',
+        icon: ''
+    }
+]
+
 export default function Side() {
     return (
-        <button>Home</button>
+        <div className="flex flex-col justify-between">
+            {
+                pages.map(({ icon, name }) => {
+                    return (
+                        <button>{name}</button>
+                    )
+                })
+            }
+        </div>
     )
 }

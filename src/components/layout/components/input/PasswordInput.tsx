0362d240a6c9da8f19e3daf7649e93f770224c9c
Ailakks <hola@cortmnzz.com>
2024-02-10 14:24:54 +0100

    feat: password input action

--- src/components/layout/components/input/PasswordInput.tsx
+++ src/components/layout/components/input/PasswordInput.tsx
@@ -1,10 +1,15 @@
+import {useState} from "react";
+import {clsx} from "clsx";
+
 export default function PasswordInput() {
+    const [visible, setVisible] = useState(false);
+
     return (
         <div className="flex items-center relative">
-            <input type="password" className="main w-full" placeholder="Password" />
-            <button className="absolute cursor-pointer right-2 p-2">
-                <i className="fa-regular fa-eye" />
-            </button>
+            <input type={visible ? 'text' : 'password'} className="main w-full" placeholder="Password" />
+            <span onClick={() => setVisible((prevState) => !prevState)} className="absolute cursor-pointer right-2 p-2">
+                <i className={clsx(visible ? 'fa-regular fa-eye-slash' : 'fa-regular fa-eye', 'text-white')} />
+            </span>
         </div>
     )
 }

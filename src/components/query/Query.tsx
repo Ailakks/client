e737913aeaf096d61688fd891e5aa1b210ced7bf
Ailakks <hola@cortmnzz.com>
2024-02-11 18:58:51 +0100

    feat: query component

---
+++ src/components/query/Query.tsx
@@ -0,0 +1,28 @@
+import {createContext} from "react";
+import LoadSpinner from "../load/spinner/LoadSpinner";
+
+export const QueryContext = createContext(null);
+
+export default function Query({ request, fallback, children }) {
+    const { loading, error, data, refetch } = request;
+
+    if (loading) {
+        return (
+            <div className="flex items-center justify-center">
+                <LoadSpinner />
+            </div>
+        )
+    }
+
+    if (error) {
+        if (fallback) {
+            return fallback;
+        }
+    }
+
+    return (
+        <QueryContext.Provider value={{ loading, data, refetch }}>
+            {children}
+        </QueryContext.Provider>
+    );
+}

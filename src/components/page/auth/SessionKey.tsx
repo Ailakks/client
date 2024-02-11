e12bc5ff086df1dfa978052a264b7a7aa12a99c5
Ailakks <hola@cortmnzz.com>
2024-02-11 19:28:26 +0100

    perf: session key page

--- src/components/page/auth/SessionKey.tsx
+++ src/components/page/auth/SessionKey.tsx
@@ -1,6 +1,11 @@
 import {gql, useLazyQuery} from "@apollo/client";
 import {useNavigate} from "react-router-dom";
 import {setToken} from "../../../wrapper/Apollo";
+import Form from "../../query/Form";
+import Input from "../../query/Input";
+import PasswordInput from "../../layout/components/input/PasswordInput";
+import LoadStatus from "../../load/LoadStatus";
+import LoadSpinner from "../../load/spinner/LoadSpinner";
 
 export default function SessionKey() {
     const navigate = useNavigate();
@@ -28,8 +33,21 @@ export default function SessionKey() {
         <div className="h-full flex flex-col">
             <div className="h-full flex justify-center items-center">
                 <div className="w-80 space-y-5">
-                    <h1>Key required</h1>
-
+                    <h1>Password required</h1>
+                    <p>Your password is required to decrypt files.</p>
+                    <hr />
+                    <Form className="space-y-2" submit={(variables) => update({ variables })}>
+                        <PasswordInput />
+                        <LoadStatus loading={loading} loader={
+                            <div className="flex items-center justify-center space-x-4">
+                                <LoadSpinner />
+                                <p>Decrypting...</p>
+                            </div>
+                        }>
+                            <button type="submit" className="main w-full">Continue</button>
+                        </LoadStatus>
+                    </Form>
+                    <a className="flex justify-center text-blue-700" href="">Forgot your password?</a>
                 </div>
             </div>
             <div className="flex justify-center space-x-16">

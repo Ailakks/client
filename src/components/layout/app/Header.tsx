d423fb721a251578909d8bc01942c15e0f1861dc
Ailakks <hola@cortmnzz.com>
2024-02-11 19:12:05 +0100

    feat: logout button

--- src/components/layout/app/Header.tsx
+++ src/components/layout/app/Header.tsx
@@ -1,8 +1,22 @@
+import {useContext} from "react";
+import {AccountContext} from "../../../wrapper/Account";
+import {useNavigate} from "react-router-dom";
+import {removeToken} from "../../../wrapper/Apollo";
+
 export default function AppHeader() {
+    const { refetch } = useContext(AccountContext);
+
+    const navigate = useNavigate();
+
+    const logout = () => {
+        removeToken();
+        refetch().then(() => navigate('/login'));
+    };
+
     return (
-        <div className="flex grow justify-between">
+        <div className="flex grow justify-between items-center">
             <input className="main" placeholder="Search" />
-            <button>Settings</button>
+            <button className="secondary" onClick={logout}>Logout</button>
         </div>
     )
 }

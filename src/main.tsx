f7c5823475d95cdcd42779fc51a997bbd0ff3830
Ailakks <hola@cortmnzz.com>
2024-02-11 19:00:38 +0100

    feat: account wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import {BrowserRouter} from "react-router-dom";
 import ApolloWrapper from "./wrapper/Apollo";
 import cookie from 'react-cookies';
+import AccountWrapper from "./wrapper/Account";
 
 export const setCookie = (name, value) => {
     cookie.save(name, value, { path: "/" });
@@ -26,7 +27,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
         <ApolloWrapper>
-            <App />
+            <AccountWrapper>
+                <App />
+            </AccountWrapper>
         </ApolloWrapper>
     </BrowserRouter>
   </React.StrictMode>,
---
+++ src/wrapper/Account.tsx
@@ -0,0 +1,47 @@
+import {createContext, useContext} from 'react';
+import Query from "../components/query/Query";
+
+export const AccountContext = createContext(null);
+
+export default function AccountWrapper({ children }) {
+    const request = useQuery(gql`
+        query CurrentUser {
+            currentUser {
+                id
+                name
+                email {
+                    value
+                    verified
+                }
+                picture {
+                    source
+                }
+                roles {
+                    id
+                    name
+                    displayName
+                    scopes
+                    __typename
+                }
+                __typename
+            }
+        }`);
+
+    return (
+        <Query request={request}>
+            <Body>
+                {children}
+            </Body>
+        </Query>
+    );
+}
+
+function Body({ children }) {
+    const { response, refetch } = useContext(QueryContext);
+
+    return (
+        <AccountContext.Provider value={{ response, refetch, logout }}>
+            {children}
+        </AccountContext.Provider>
+    );
+}

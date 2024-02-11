4294345468f3ebf27289702d4e9ac2c3ace3a547
Ailakks <hola@cortmnzz.com>
2024-02-11 19:03:21 +0100

    feat: key wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 import cookie from 'react-cookies';
 import ApolloWrapper from "./wrapper/Apollo";
 import AccountWrapper from "./wrapper/Account";
+import KeyWrapper from "./wrapper/Key";
 
 export const setCookie = (name, value) => {
     cookie.save(name, value, { path: "/" });
@@ -28,7 +29,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <ApolloWrapper>
             <AccountWrapper>
-                <App />
+                <KeyWrapper>
+                    <App />
+                </KeyWrapper>
             </AccountWrapper>
         </ApolloWrapper>
     </BrowserRouter>
---
+++ src/wrapper/Key.tsx
@@ -0,0 +1,34 @@
+import {createContext, useContext} from 'react';
+import Query, {QueryContext} from "../components/query/Query";
+import {gql, useQuery} from "@apollo/client";
+
+export const KeyContext = createContext(null);
+
+export default function KeyWrapper({ children }) {
+    const request = useQuery(gql`
+        query {
+            decryptedKey {
+                public
+                private
+                __typename
+            }
+        }`);
+
+    return (
+        <Query request={request}>
+            <Body>
+                {children}
+            </Body>
+        </Query>
+    );
+}
+
+function Body({ children }) {
+    const { response, refetch } = useContext(QueryContext);
+
+    return (
+        <KeyContext.Provider value={{ response, refetch }}>
+            {children}
+        </KeyContext.Provider>
+    );
+}

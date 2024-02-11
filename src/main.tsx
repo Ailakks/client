373f89ee6a183f35e86bd1274dc2a261501081ae
Ailakks <hola@cortmnzz.com>
2024-02-11 19:05:30 +0100

    feat: logged wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 import cookie from 'react-cookies';
 import ApolloWrapper from "./wrapper/Apollo";
 import AccountWrapper from "./wrapper/Account";
-import KeyWrapper from "./wrapper/Key";
+import LoggedWrapper from "./wrapper/Logged";
 
 export const setCookie = (name, value) => {
     cookie.save(name, value, { path: "/" });
@@ -29,9 +29,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <ApolloWrapper>
             <AccountWrapper>
-                <KeyWrapper>
+                <LoggedWrapper>
                     <App />
-                </KeyWrapper>
+                </LoggedWrapper>
             </AccountWrapper>
         </ApolloWrapper>
     </BrowserRouter>
--- src/wrapper/logged/Key.tsx
+++ src/wrapper/logged/Key.tsx
@@ -1,5 +1,5 @@
 import {createContext, useContext} from 'react';
-import Query, {QueryContext} from "../components/query/Query";
+import Query, {QueryContext} from "../../components/query/Query";
 import {gql, useQuery} from "@apollo/client";
 
 export const KeyContext = createContext(null);

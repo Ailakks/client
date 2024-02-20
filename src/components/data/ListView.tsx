c5ba5da9a5b8de24df8b03211d572aa0eec4a630
Ailakks <hola@cortmnzz.com>
2024-02-21 00:13:52 +0100

    feat: folder list view

---
+++ src/components/data/ListView.tsx
@@ -0,0 +1,12 @@
+export default function ListView() {
+    return (
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Date</th>
+                <th>Size</th>
+                <th>Options</th>
+            </tr>
+        </table>
+    )
+}

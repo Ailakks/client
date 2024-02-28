37ed72dba6f80e5bd3fa4e58ae4e01df43195ffe
Ailakks <hola@cortmnzz.com>
2024-02-28 19:50:21 +0100

    feat: list view file check

--- src/components/data/ListView.tsx
+++ src/components/data/ListView.tsx
@@ -9,6 +9,7 @@ export default function ListView() {
         <table className="w-full text-white">
             <tbody>
                 <tr className="text-left">
+                    <th />
                     <th>Name</th>
                     <th>Date</th>
                     <th>Size</th>
@@ -25,6 +26,7 @@ function FileItem() {
 
     return (
         <tr>
+            <FileCheck />
             <td>{name}</td>
             <td>{date}</td>
             <td>{size}</td>
@@ -34,5 +36,17 @@ function FileItem() {
                 </div>
             </td>
         </tr>
+    )
+}
+
+function FileCheck() {
+    const { item } = useContext(ListContext);
+
+    const toggle = (event) => {
+        const status = event.target.checked;
+    }
+
+    return (
+        <input type="checkbox" onClick={toggle} />
     )
 }

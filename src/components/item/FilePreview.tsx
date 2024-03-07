9eca8e6a3f05fc4dd410b5fd35c55928c0dd53a8
Ailakks <hola@cortmnzz.com>
2024-03-07 13:27:28 +0100

    feat: use axios hook

--- src/components/item/FilePreview.tsx
+++ src/components/item/FilePreview.tsx
@@ -1,8 +1,12 @@
 import {useContext} from "react";
 import {ListContext} from "../list/List";
+import {AxiosContext} from "../../wrapper/Axios";
 
 export default function FilePreview() {
+    const { useClient } = useContext(AxiosContext);
     const { item: { id } } = useContext(ListContext);
+
+    const request = useClient(`file/${id}`);
 
     return (
         <p>{id}</p>
--- src/wrapper/Axios.tsx
+++ src/wrapper/Axios.tsx
@@ -1,21 +1,26 @@
 import {createContext, useContext} from 'react';
 import {CookiesContext} from "./Cookies";
 import axios from "axios";
+import {makeUseAxios} from "axios-hooks";
 
 export const AxiosContext = createContext(null);
 
 export default function AxiosWrapper({ children }) {
     const { getToken } = useContext(CookiesContext);
 
-    const axiosClient = axios.create({
+    const client = axios.create({
         baseURL: import.meta.env.VITE_API_REST_BASE_URL,
         headers: {
             'Authorization': `Bearer ${getToken()}`
         },
     });
 
+    const useClient = makeUseAxios({
+        axios: client,
+    });
+
     return (
-        <AxiosContext.Provider value={{ axiosClient }}>
+        <AxiosContext.Provider value={{ client, useClient }}>
             {children}
         </AxiosContext.Provider>
     );

d859e4bacb207542e7016e2194bdb9db31b985cc
Ailakks <hola@cortmnzz.com>
2024-03-07 13:44:11 +0100

    feat: blob preview

--- src/components/item/FilePreview.tsx
+++ src/components/item/FilePreview.tsx
@@ -1,17 +1,32 @@
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import {ListContext} from "../list/List";
 import {AxiosContext} from "../../wrapper/Axios";
-import Request from "../query/Request";
+import Request, {RequestContext} from "../query/Request";
 
 export default function FilePreview() {
     const { useClient } = useContext(AxiosContext);
     const { item: { id } } = useContext(ListContext);
 
-    const request = useClient(`file/${id}`);
+    const request = useClient({ url: `file/${id}`, responseType: 'arraybuffer' });
 
     return (
         <Request request={request}>
-            
+            <Body />
         </Request>
+    )
+}
+
+function Body() {
+    const { data } = useContext(RequestContext);
+    const { item: { source: { meta: { mime } } } } = useContext(ListContext);
+
+    const [blob, setBlob] = useState(null);
+
+    useEffect(() => {
+        setBlob(URL.createObjectURL(new Blob([data], { type: mime })));
+    }, [data]);
+
+    return (
+        <iframe src={blob} />
     )
 }

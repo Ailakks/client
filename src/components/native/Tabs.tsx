156c1a9231992b31c94c06f6271e1381e6e6bb7c
Ailakks <hola@cortmnzz.com>
2024-03-09 21:49:18 +0100

    feat: tabs component

---
+++ src/components/native/Tabs.tsx
@@ -0,0 +1,13 @@
+import React, {createContext, useState} from "react";
+
+export const TabsContext = createContext();
+
+export default function Tabs({ children }) {
+    const [current, setCurrent] = useState();
+
+    return (
+        <TabsContext.Provider value={{ current, setCurrent }}>
+            {children}
+        </TabsContext.Provider>
+    )
+}

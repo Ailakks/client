23cc69f36d05977f9d7f4599f8617f7675b9f17b
Ailakks <hola@cortmnzz.com>
2024-03-10 20:46:36 +0100

    feat: upload queue status

--- src/components/data/queue/QueueList.tsx
+++ src/components/data/queue/QueueList.tsx
@@ -1,9 +1,25 @@
 import Modal from "../../ui/Modal";
+import {useContext} from "react";
+import {QueueContext} from "../../../wrapper/api/Queue";
+import List, {ListContext} from "../../list/List";
 
 export default function QueueList() {
+    const { queue } = useContext(QueueContext);
+
     return (
         <Modal title="Queue">
-            <p>test</p>
+            <List list={queue}><Item /></List>
         </Modal>
+    )
+}
+
+function Item() {
+    const { item: { file: { name }, event: { progress, total } } } = useContext(ListContext);
+
+    return (
+        <div>
+            <p>{name}</p>
+            <p>{progress}/{total}</p>
+        </div>
     )
 }
--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -9,7 +9,7 @@ export default function Modal({ title, children }) {
     };
 
     return (
-        <div className="absolute bottom-0 right-0 bg-gray-500 p-4 m-6 rounded-md min-w-80">
+        <div className="absolute bottom-0 right-0 bg-gray-500 p-4 m-6 rounded-md min-w-80 z-10">
             <div className="flex flex-col space-y-4">
                 <div className="flex justify-between items-center h-5">
                     <p>{title}</p>

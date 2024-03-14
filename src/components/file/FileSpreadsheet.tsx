a0736fe19fc362311b76cfb2cd1a5e22fca06bca
Ailakks <hola@ailakks.dev>
2024-03-14 23:19:44 +0100

    feat: spreadsheet item list

--- src/components/file/FileSpreadsheet.tsx
+++ src/components/file/FileSpreadsheet.tsx
@@ -3,43 +3,45 @@ import {useContext} from "react";
 import Query, {QueryContext} from "../query/Query";
 import {gql, useQuery} from "@apollo/client";
 import {ScopesDataContext} from "../context/Scopes";
-import {Table, TableBody, TableCell, TableColumn, TableHeader, TableRow} from "@nextui-org/react";
+import List, {ListContext} from "../list/List";
 
 export default function FileSpreadsheet() {
-    const { item: { id } } = useContext(ScopesDataContext);
+    const {item: {id}} = useContext(ScopesDataContext);
 
     const request = useQuery(gql`
-        query Login($file: String!) {
-            generateSheet(payload: {
-                file: $file,
-            }) {
-                file {
-                    id
-                    __typename
-                }
-                result {
-                    list {
-                        code
-                        quantity
-                        tax_amount
-                        tax_rate
-                        price_unit
-                        price_total
+                query Login($file: String!) {
+                    generateSheet(payload: {
+                        file: $file,
+                    }) {
+                        file {
+                            id
+                            __typename
+                        }
+                        result {
+                            list {
+                                code
+                                quantity
+                                tax_amount
+                                tax_rate
+                                price_unit
+                                price_total
+                                __typename
+                            }
+                            __typename
+                        }
                         __typename
                     }
-                    __typename
-                }
-                __typename
+                }`, {
+            variables: {
+                file: id
             }
-        }`, { variables: {
-            file: id
-        } }
+        }
     );
 
     return (
         <IntegrationMeta name="Spreadsheet">
             <Query request={request}>
-                <Body />
+                <Body/>
             </Query>
         </IntegrationMeta>
     );
@@ -48,37 +50,33 @@ export default function FileSpreadsheet() {
 function Body() {
     const { data: { generateSheet: { result: { list } } } } = useContext(QueryContext);
 
-    const columns = [
-        {
-            key: "name",
-            label: "NAME",
-        },
-        {
-            key: "role",
-            label: "ROLE",
-        },
-        {
-            key: "status",
-            label: "STATUS",
-        },
-    ];
-
     return (
         <table>
             <thead>
             <tr>
-                <th>
-                    <p>header</p>
-                </th>
             </tr>
             </thead>
             <tbody>
             <tr>
-                <td>
-                    <p>body</p>
-                </td>
+                <List list={list}><Item /></List>
             </tr>
             </tbody>
         </table>
+    )
+}
+
+function Item() {
+    const { item } = useContext(ListContext);
+
+    return (
+        <th>
+            {
+                    Object.entries(item).map(([key, value]) => (
+                    <div key={key}>
+                        <p>{value}</p>
+                    </div>
+                ))
+            }
+        </th>
     )
 }

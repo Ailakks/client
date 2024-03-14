8273e083167c9cdba613fdccf4eb5b7c46c3fee8
Ailakks <hola@ailakks.dev>
2024-03-14 23:10:18 +0100

    feat: spreadsheet nextui table

--- src/components/file/FileSpreadsheet.tsx
+++ src/components/file/FileSpreadsheet.tsx
@@ -3,6 +3,7 @@ import {useContext} from "react";
 import Query, {QueryContext} from "../query/Query";
 import {gql, useQuery} from "@apollo/client";
 import {ScopesDataContext} from "../context/Scopes";
+import {Table, TableBody, TableCell, TableColumn, TableHeader, TableRow} from "@nextui-org/react";
 
 export default function FileSpreadsheet() {
     const { item: { id } } = useContext(ScopesDataContext);
@@ -47,7 +48,35 @@ export default function FileSpreadsheet() {
 function Body() {
     const { data: { generateSheet: { result: { list } } } } = useContext(QueryContext);
 
+    const columns = [
+        {
+            key: "name",
+            label: "NAME",
+        },
+        {
+            key: "role",
+            label: "ROLE",
+        },
+        {
+            key: "status",
+            label: "STATUS",
+        },
+    ];
+
     return (
-        <p>{JSON.stringify(list)}</p>
+        <Table aria-label="Example table with dynamic content">
+            <TableHeader>
+                <TableColumn>
+                    <p>header</p>
+                </TableColumn>
+            </TableHeader>
+            <TableBody>
+                <TableRow>
+                    <TableCell>
+                        <p>body</p>
+                    </TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
     )
 }

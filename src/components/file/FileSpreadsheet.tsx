5dbc0b49d33b8bf82a13b413a9c1fd14c17c687f
Ailakks <hola@ailakks.dev>
2024-03-14 21:07:45 +0100

    feat: spreadsheet generate query

--- src/components/file/FileSpreadsheet.tsx
+++ src/components/file/FileSpreadsheet.tsx
@@ -1,9 +1,48 @@
 import IntegrationMeta from "../item/IntegrationMeta";
+import {useContext} from "react";
+import Query, {QueryContext} from "../query/Query";
+import {gql, useQuery} from "@apollo/client";
 
 export default function FileSpreadsheet() {
+    const request = useQuery(gql`
+        query Login($file: String!) {
+            generateSheet(payload: {
+                file: $file,
+            }) {
+                file {
+                    id
+                    __typename
+                }
+                result {
+                    list {
+                        code
+                        quantity
+                        tax_amount
+                        tax_rate
+                        price_unit
+                        price_total
+                        __typename
+                    }
+                    __typename
+                }
+                __typename
+            }
+        }`
+    );
+
+    return (
+        <Query request={request}>
+            <Body />
+        </Query>
+    );
+}
+
+function Body() {
+    const { data } = useContext(QueryContext);
+
     return (
         <IntegrationMeta name="Spreadsheet">
-            <p>test</p>
+            <p>{JSON.stringify(data)}</p>
         </IntegrationMeta>
     )
 }

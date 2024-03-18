1d707dd81105edf8483ee9a693726bce6cb40fa4
Ailakks <hola@ailakks.dev>
2024-03-18 23:39:33 +0100

    feat: file sheet query

--- src/components/file/FileSpreadsheet.tsx
+++ src/components/file/FileSpreadsheet.tsx
@@ -6,6 +6,7 @@ import Tabs from "../native/Tabs";
 import Tab from "../native/Tab";
 import TabContent from "../native/TabContent";
 import IntegrationMeta from "../item/IntegrationMeta";
+import {ScopesDataContext} from "../context/Scopes";
 
 export const TemplateContext = createContext();
 
@@ -45,16 +46,60 @@ function Body() {
 }
 
 function TemplateTab() {
-    const { item: { id, name } } = useContext(ListContext);
+    const { item } = useContext(ListContext);
+
+    const { name } = item;
 
     return (
         <Tab>
             <p>{name}</p>
             <TabContent>
-                <TemplateContext.Provider value={{ id }}>
-
+                <TemplateContext.Provider value={{ item }}>
+                    <TemplateSheet />
                 </TemplateContext.Provider>
             </TabContent>
         </Tab>
     )
+}
+
+function TemplateSheet() {
+    const { item: { id } } = useContext(ScopesDataContext);
+
+    const request = useQuery(gql`
+        query GenerateSheet($file: String!) {
+            generateSheet(payload: {
+                file: $file
+            }) {
+                id
+                file {
+                    id
+                    __typename
+                }
+                result {
+                    list {
+                        code
+                        quantity
+                        tax_amount
+                        tax_rate
+                        price_unit
+                        price_total
+                        __typename
+                    }
+                    __typename
+                }
+                __typename
+            }
+        }`,
+        {
+            variables: {
+                file: id
+            }
+        }
+    );
+
+    return (
+        <Query request={request}>
+            <Body/>
+        </Query>
+    );
 }

805a387ee69c6ba1cb03c94845f684eab7e24261
Ailakks <hola@ailakks.dev>
2024-03-28 21:35:50 +0100

    feat: pricing page

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Logout from "./components/page/auth/Logout";
 import FolderPage from "./components/page/FolderPage";
 import Trash from "./components/page/Trash";
 import NotFoundFallback from "./components/page/fallback/NotFoundFallback";
+import PricingPage from "./components/page/PricingPage";
 
 export default function App() {
     const location = useLocation();
@@ -15,15 +16,14 @@ export default function App() {
     return (
         <Routes location={location}>
             <Route path="/" element={<AppLayout><HomePage /></AppLayout>} />
-
-            <Route path="/trash" element={<AppLayout><Trash /></AppLayout>} />
+            <Route path="/pricing" element={<AppLayout><PricingPage /></AppLayout>} />
 
             <Route path="/folder/:id" element={<AppLayout><FolderPage /></AppLayout>} />
-
-            <Route path="/key" element={<AuthLayout><Key /></AuthLayout>} />
-            <Route path="/logout" element={<AuthLayout><Logout /></AuthLayout>} />
+            <Route path="/trash" element={<AppLayout><Trash /></AppLayout>} />
 
             <Route path="/login" element={<AuthLayout><Login /></AuthLayout>} />
+            <Route path="/key" element={<AuthLayout><Key /></AuthLayout>} />
+            <Route path="/logout" element={<AuthLayout><Logout /></AuthLayout>} />
 
             <Route path="/*" element={<AuthLayout><NotFoundFallback /></AuthLayout>} />
         </Routes>

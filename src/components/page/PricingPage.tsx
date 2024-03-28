b0950251cb30eb354e5a3840f6d94ccb154824b5
Ailakks <hola@ailakks.dev>
2024-03-28 21:40:59 +0100

    feat: pricing plans list

--- src/components/page/PricingPage.tsx
+++ src/components/page/PricingPage.tsx
@@ -1,7 +1,47 @@
+import {useContext} from "react";
+import {LanguageContext} from "../../wrapper/lang/LanguageWrapper";
+import List, {ListContext} from "../list/List";
+
 export default function PricingPage() {
+    const { translate } = useContext(LanguageContext);
+
+    const plans = [
+        {
+            stripe: 1,
+            features: [
+                {
+                    label: translate("pricing.plan.feature.convert.label")
+                }
+            ]
+        }
+    ]
+
     return (
         <div>
-            <h1>Pricing</h1>
+            <div className="bg-blue-500 px-10 py-20 text-center rounded-xl">
+                <h1>{translate("pricing.title")}</h1>
+            </div>
+            <List list={plans} children={<Plan />} />
+        </div>
+    );
+}
+
+function Plan() {
+    const { item: { stripe, features } } = useContext(ListContext);
+
+    return (
+        <div>
+            <List list={features} children={<Feature />} />
+        </div>
+    );
+}
+
+function Feature() {
+    const { item: { label } } = useContext(ListContext);
+
+    return (
+        <div>
+            <p>{label}</p>
         </div>
     );
 }

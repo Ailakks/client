a3502401be35c3a726629883f26a251ca0c74799
Ailakks <hola@ailakks.dev>
2024-04-08 19:39:51 +0200

    feat: toast handle

--- src/wrapper/ui/Toast.tsx
+++ src/wrapper/ui/Toast.tsx
@@ -2,8 +2,27 @@ import {createContext, useState} from 'react';
 
 export const ToastContext = createContext(null);
 
+export const ToastType = {
+    DONE: { code: 200, title: 'Ok', type: 'ok' },
+    CREATED: { code: 201, title: 'Done', type: 'created' },
+    ERROR: { code: 500, title: 'Something went wrong', type: 'error' },
+    NOT_FOUND: { code: 404, title: 'That could not be found', type: 'error' }
+};
+
 export default function ToastWrapper({ children }) {
     const [list, setList] = useState([]);
+
+    const handle = (error: any) => {
+        if (!error.response) {
+            return;
+        }
+
+        const { response: { status } } = error;
+
+        const { title, type } = Object.values(ToastType).find(({ code }) => code === status) ?? ToastType.ERROR;
+
+        add(title, type);
+    };
 
     const clear = () => {
         setList([]);
@@ -13,7 +32,7 @@ export default function ToastWrapper({ children }) {
         setList(list.filter(({ id: current }) => current !== id));
     }
 
-    const add = (title: string, type: any) => {
+    const add = (title: string, type: ToastType) => {
         const toast = { date: Date.now(), title, type };
 
         setList([...list, toast]);
@@ -24,7 +43,7 @@ export default function ToastWrapper({ children }) {
     }
 
     return (
-        <ToastContext.Provider value={{ clear, list, setList }}>
+        <ToastContext.Provider value={{ add, remove, clear, handle, list, setList }}>
             {children}
         </ToastContext.Provider>
     );

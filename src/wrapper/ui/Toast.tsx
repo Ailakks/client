8c119238d4bbcfc5d4886862e19b42f4c08a4768
Ailakks <hola@ailakks.dev>
2024-04-08 19:37:44 +0200

    feat: toast list management

--- src/wrapper/ui/Toast.tsx
+++ src/wrapper/ui/Toast.tsx
@@ -9,6 +9,20 @@ export default function ToastWrapper({ children }) {
         setList([]);
     }
 
+    const remove = (id: string) => {
+        setList(list.filter(({ id: current }) => current !== id));
+    }
+
+    const add = (title: string, type: any) => {
+        const toast = { date: Date.now(), title, type };
+
+        setList([...list, toast]);
+
+        setTimeout(() => {
+            remove(toast);
+        }, 3000);
+    }
+
     return (
         <ToastContext.Provider value={{ clear, list, setList }}>
             {children}

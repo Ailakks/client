5089da4963c7b7a8a2d16748e6b2881ce1e7db8e
Ailakks <hola@ailakks.dev>
2024-04-09 20:28:13 +0200

    feat: widget component

---
+++ src/components/page/dash/item/Widget.tsx
@@ -0,0 +1,105 @@
+import style from './style.module.css';
+
+import {createContext, useContext, useState} from "react";
+import jsonpath from "jsonpath";
+
+export const WidgetContext = createContext(null);
+
+export default function Widget({ panelRef, collapsed, children }) {
+    const { layout, setLayout } = useContext(LayoutContext);
+    const { widgets } = useContext(WidgetsContext);
+    const { path } = useContext(PathContext);
+    const { widgetList } = useContext(GridProviderContext);
+    const { index } = useContext(GridViewContext);
+
+    const [metadata, setMetadata] = useState(null);
+
+    if (!metadata) {
+        return (
+            <WidgetContext.Provider value={{ setMetadata }}>
+                {children}
+            </WidgetContext.Provider>
+        );
+    }
+
+    const { name } = metadata;
+
+    const add = (id) => {
+        const component = getComponent(id);
+        const current = jsonpath.value(layout, path);
+
+        const updatedLayout = [ ...layout ];
+
+        jsonpath.apply(updatedLayout, path, () => [component, ...current]);
+
+        setLayout(updatedLayout);
+    };
+
+    const replace = (id) => {
+        const component = getComponent(id);
+        const updatedLayout = [ ...layout ];
+
+        jsonpath.apply(updatedLayout, `${path}[${index}]`, () => component);
+
+        setLayout(updatedLayout);
+    };
+
+    const remove = () => {
+        const updatedLayout = [ ...layout ];
+        const current = jsonpath.value(layout, path);
+
+        if (!current) {
+            return;
+        }
+
+        current.splice(index, 1);
+
+        if (current.length < 1) {
+            const array = path.split('.');
+
+            array.pop();
+
+            const last = array.pop();
+            const index = last.match(/\[(\d+)]/)[1];
+            const parent = [...array, last.replace(/\[\d+]/g, '')].join('.');
+
+            const updated = jsonpath.value(layout, parent);
+            updated.splice(index, 1);
+
+            jsonpath.apply(updatedLayout, parent, () => updated);
+
+            const root = updatedLayout[0];
+            const first = root.row ?? root.column;
+
+            if (first.length < 1) {
+                return;
+            }
+        }
+
+        jsonpath.apply(updatedLayout, path, () => current);
+
+        setLayout(updatedLayout);
+    };
+
+    const collapse = () => {
+        if (!panelRef.current) {
+            return;
+        }
+
+        if (panelRef.current.isCollapsed()) {
+            panelRef.current.expand();
+        } else {
+            panelRef.current.collapse();
+        }
+    };
+
+    const getComponent = (id) => {
+        return widgets[widgetList.map(({ id }) => id).indexOf(id)];
+    };
+
+    return (
+        <WidgetContext.Provider value={{ metadata, setMetadata, replace }}>
+            {!collapsed && children}
+        </WidgetContext.Provider>
+    )
+}
--- src/components/page/dash/widgets/ChatWidget.tsx
+++ src/components/page/dash/widgets/ChatWidget.tsx
@@ -3,8 +3,6 @@ import {useContext, useEffect} from "react";
 export default function ChatWidget() {
     const { metadata, setMetadata } = useContext(WidgetContext);
 
-    const { section } = useContext(PlatformFilterContext);
-
     useEffect(() => {
         setMetadata({ id: 'chat', name: 'Chat', icon: 'fa-regular fa-message-lines', scopes: [{ id: "message" }], platforms: ["youtube", "twitch", "tiktok", "x", "kick"] });
     }, []);

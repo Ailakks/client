ddebc04f93cc629ab68af68178b397d1fc616b63
Ailakks <hola@ailakks.dev>
2024-04-09 19:59:26 +0200

    feat: google auth redirect

--- src/components/page/auth/Login.tsx
+++ src/components/page/auth/Login.tsx
@@ -1,7 +1,7 @@
 import PasswordInput from "../../layout/components/input/PasswordInput";
 import Form from "../../query/Form";
 import Input from "../../query/Input";
-import {gql, useLazyQuery} from "@apollo/client";
+import {gql, useLazyQuery, useQuery} from "@apollo/client";
 import LoadStatus from "../../load/LoadStatus";
 import LoadSpinner from "../../load/spinner/LoadSpinner";
 import {useNavigate} from "react-router-dom";
@@ -9,23 +9,32 @@ import {useContext, useEffect} from "react";
 import {AccountContext} from "../../../wrapper/Account";
 import {CookiesContext} from "../../../wrapper/tool/Cookies";
 import {LanguageContext} from "../../../wrapper/lang/LanguageWrapper";
+import Query, {QueryContext} from "../../query/Query";
 
 export default function Login() {
-    const { translate } = useContext(LanguageContext);
+    const {translate} = useContext(LanguageContext);
 
     const navigate = useNavigate();
 
-    const { setToken } = useContext(CookiesContext);
+    const {setToken} = useContext(CookiesContext);
+    const {data} = useContext(AccountContext);
 
-    const { data } = useContext(AccountContext);
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get('code');
 
     useEffect(() => {
-       if (data) {
-           navigate('/');
-       }
+        if (data) {
+            navigate('/');
+        }
     }, [data]);
 
-    const [update, { loading }] = useLazyQuery(gql`
+    if (code) {
+        return (
+            <OAuth />
+        )
+    }
+
+    const [update, {loading}] = useLazyQuery(gql`
         query Login($email: String!, $password: String!) {
             login(payload: {
                 email: $email,
@@ -35,7 +44,7 @@ export default function Login() {
                 __typename
             }
         }`, {
-        onCompleted: ({ login: { token } }) => {
+        onCompleted: ({login: {token}}) => {
             setToken(token);
             navigate('/');
 
@@ -50,7 +59,7 @@ export default function Login() {
             <div className="h-full flex justify-center items-center">
                 <div className="w-80 space-y-5">
                     <h1>{translate("auth.login.title")}</h1>
-                    <GoogleButton />
+                    <GoogleButton/>
                     <hr/>
                     <Form className="space-y-2" submit={(variables) => update({variables})}>
                         <Input name="email" type="email" className="main w-full"
@@ -81,18 +90,17 @@ export default function Login() {
     )
 }
 
-
 function GoogleButton() {
-    const { translate } = useContext(LanguageContext);
+    const {translate} = useContext(LanguageContext);
 
-    const [update, { loading }] = useLazyQuery(gql`
+    const [update, {loading}] = useLazyQuery(gql`
         query Generate {
             generate {
                 url
                 __typename
             }
         }`, {
-        onCompleted: ({ generate: { url } }) => {
+        onCompleted: ({generate: {url}}) => {
             window.location.href = url;
         },
         onError: () => {
@@ -100,9 +108,65 @@ function GoogleButton() {
     });
 
     return (
-        <button className="main w-full space-x-2" onClick={update}>
-            <i className="fa-brands fa-google"/>
-            <span>{translate("auth.login.quick.google")}</span>
-        </button>
+        <LoadStatus loading={loading} loader={
+            <div className="inline justify-center">
+                <LoadSpinner/>
+                <p>{translate("auth.login.form.submit.loading")}</p>
+            </div>
+        }>
+            <button className="main w-full space-x-2" onClick={update}>
+                <i className="fa-brands fa-google"/>
+                <span>{translate("auth.login.quick.google")}</span>
+            </button>
+        </LoadStatus>
+    )
+}
+
+function OAuth() {
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get('code');
+
+    const request = useQuery(gql`
+                query Login($code: String!) {
+                    login(payload: {
+                        code: $code
+                    }) {
+                        token
+                        __typename
+                    }
+                }`,
+        {
+            variables: {
+                code: code
+            }
+        });
+
+    return (
+        <Query request={request}>
+            <OAuthLogin />
+        </Query>
+    )
+}
+
+function OAuthLogin() {
+    const { translate } = useContext(LanguageContext);
+
+    const { data: { login: { token } } } = useContext(QueryContext);
+
+    const navigate = useNavigate();
+
+    const { setToken } = useContext(CookiesContext);
+
+    useEffect(() => {
+        setToken(token);
+        navigate('/');
+
+        window.location.reload();
+    }, [token]);
+
+    return (
+        <div className="h-full flex justify-center items-center">
+            <p>{translate("auth.login.oauth.loading")}</p>
+        </div>
     )
 }

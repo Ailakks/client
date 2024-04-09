e27d275a409fbfdbb3babcee8d5fc3d5b531a38e
Ailakks <hola@ailakks.dev>
2024-04-09 21:26:05 +0200

    feat: widget window header

--- src/components/page/dash/item/Window.tsx
+++ src/components/page/dash/item/Window.tsx
@@ -6,6 +6,20 @@ export default function Window({ children }) {
 
     return (
         <div>
+            <div className="flex h-10 bg-gray-300">
+                <button onClick={replace}>
+                    <i className="fa-regular ellipsis"/>
+                </button>
+                <button onClick={add}>
+                    <i className="fa-regular fa-plus"/>
+                </button>
+                <button onClick={collapse}>
+                    <i className={collapsed ? "fa-regular fa-angle-down" : "fa-regular fa-angle-up"} />
+                </button>
+                <button onClick={remove}>
+                    <i className="fa-regular fa-xmark" />
+                </button>
+            </div>
             {!collapsed && children}
         </div>
     )

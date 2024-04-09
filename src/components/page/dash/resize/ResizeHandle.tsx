4149993d15ea3530050138ffa1dd07318bc91820
Ailakks <hola@ailakks.dev>
2024-04-09 21:55:40 +0200

    feat: resize handle active style

--- src/components/page/dash/resize/ResizeHandle.tsx
+++ src/components/page/dash/resize/ResizeHandle.tsx
@@ -2,6 +2,6 @@ import {PanelResizeHandle} from "react-resizable-panels";
 
 export default function ResizeHandle() {
     return (
-        <PanelResizeHandle className="basis-2 bg-orange-500" />
+        <PanelResizeHandle className="basis-2 bg-gray-900 resize-handle" />
     )
 }

02de9d3cb083b75ead443573d1623916d541c6db
Ailakks <hola@ailakks.dev>
2024-04-15 00:43:15 +0200

    perf: layout head

--- src/components/layout/dash/Dash.tsx
+++ src/components/layout/dash/Dash.tsx
@@ -2,17 +2,26 @@ import {Panel, PanelGroup} from "react-resizable-panels";
 import ResizeHandle from "../../page/dash/resize/ResizeHandle";
 import AppHead from "../app/Head";
 import AppSide from "../app/Side";
+import AppHeader from "../app/Header";
 
 export default function DashLayout({ children }) {
     return (
         <PanelGroup direction="vertical">
             <Panel defaultSize={8} minSize={8} maxSize={8} collapsible>
+                <PanelGroup direction="horizontal">
+                    <Panel defaultSize={12}>
+                        <AppHead />
+                    </Panel>
+                    <Panel>
+                        <AppHeader />
+                    </Panel>
+                </PanelGroup>
                 <AppHead />
             </Panel>
             <ResizeHandle />
             <Panel>
                 <PanelGroup direction="horizontal">
-                    <Panel defaultSize={12} minSize={12} maxSize={12} collapsible>
+                    <Panel defaultSize={12} collapsible>
                         <AppSide />
                     </Panel>
                     <ResizeHandle />

0cd938274b1ed77571ef0492d0e2abde97f41745
Ailakks <hola@ailakks.dev>
2024-04-14 17:22:05 +0200

    perf: widget drop string serialization

--- src/components/page/dash/item/WidgetDroppable.tsx
+++ src/components/page/dash/item/WidgetDroppable.tsx
@@ -1,28 +1,33 @@
 import {createContext, useContext} from "react";
-import {LayoutContext, WidgetsContext} from "../grid/GridView";
+import {LayoutContext} from "../grid/GridView";
 import {PathContext} from "../grid/GridRender";
-import {ListContext} from "../../../list/List";
 import Droppable from "../../../drop/Droppable";
 import jsonpath from "jsonpath";
+import {GridProviderContext} from "../GridProvider";
 
 export const WidgetDroppableContext = createContext(null);
 
 export default function WidgetDroppable({ children }) {
     const { layout, setLayout } = useContext(LayoutContext);
     const { path } = useContext(PathContext);
-    const { item } = useContext(ListContext);
+
+    const { list, widgets } = useContext(GridProviderContext);
 
     const add = (data) => {
         const newLayout = [...layout];
 
-        const { border: { type } } = data;
+        const { item: { id }, border: { type } } = data;
 
         const current = jsonpath.value(layout, path);
 
-        jsonpath.apply(newLayout, path, () => [{ [type]: [...current, item] }]);
+        jsonpath.apply(newLayout, path, () => [{ [type]: [...current, id] }]);
 
         setLayout(newLayout);
     }
+
+    const getComponent = (id) => {
+        return list[widgets.map(({ id }) => id).indexOf(id)];
+    };
 
     return (
         <WidgetDroppableContext.Provider value={{ children }}>

53f3ed5bb71962ca6c08a4312b34c5afff4ec152
Ailakks <hola@ailakks.dev>
2024-04-14 13:14:00 +0200

    perf: widget list style

--- src/components/page/dash/view/WidgetList.tsx
+++ src/components/page/dash/view/WidgetList.tsx
@@ -7,7 +7,7 @@ export default function WidgetList() {
     const { widgetList } = useContext(GridProviderContext);
 
     return (
-        <div className="h-full bg-gray-500 space-y-2">
+        <div className="h-full bg-gray-500 space-y-2 p-2">
             <List list={widgetList}><Item /></List>
         </div>
     )
@@ -17,7 +17,7 @@ function Item() {
     const { item: { name, icon } } = useContext(ListContext);
 
     return (
-        <div>
+        <div className="flex justify-center items-center w-full aspect-square bg-gray-700 rounded cursor-pointer">
             <i className={icon} />
         </div>
     )

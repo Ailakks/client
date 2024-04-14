7c5c39e891cb4e1e277713693652d3b1d74045fd
Ailakks <hola@ailakks.dev>
2024-04-14 13:29:06 +0200

    feat: widget droppable

---
+++ src/components/page/dash/item/WidgetDroppable.tsx
@@ -0,0 +1,38 @@
+import {createContext, useContext} from "react";
+import jsonpath from "jsonpath";
+import {LayoutContext, WidgetsContext} from "../grid/GridView";
+import {PathContext} from "../grid/GridRender";
+import {ListContext} from "../../../list/List";
+
+export const WidgetDroppableContext = createContext(null);
+
+export default function WidgetDroppable({ children }) {
+    const { layout, setLayout } = useContext(LayoutContext);
+    const { widgets } = useContext(WidgetsContext);
+    const { path } = useContext(PathContext);
+    const { index } = useContext(ListContext);
+
+    const add = (data) => {
+        const newLayout = [...layout];
+
+        const { item: { key }, border: { apply } } = data;
+        const component = widgets[key];
+
+        const thisPath = `${path}[${index}]`;
+        const current = jsonpath.value(layout, thisPath);
+
+        const replace = apply(current, component);
+
+        jsonpath.apply(newLayout, thisPath, () => replace);
+
+        setLayout(newLayout);
+    }
+
+    return (
+        <WidgetDroppableContext.Provider value={{ children }}>
+            <Droppable action={add}>
+                {children}
+            </Droppable>
+        </WidgetDroppableContext.Provider>
+    )
+}

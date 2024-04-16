8f8a89f4e1f86247b6a184ef8c6bd74be36397ea
Ailakks <hola@ailakks.dev>
2024-04-16 19:59:53 +0200

    feat: preview widget list

--- src/components/page/dash/widgets/PreviewWidget.tsx
+++ src/components/page/dash/widgets/PreviewWidget.tsx
@@ -1,5 +1,7 @@
 import {useContext, useEffect} from "react";
 import {WidgetDataContext} from "../item/Widget";
+import {ChannelContext} from "../../../../wrapper/api/Channel";
+import List, {ListContext} from "../../../list/List";
 
 export default function PreviewWidget() {
     const { metadata, setMetadata } = useContext(WidgetDataContext);
@@ -9,8 +11,24 @@ export default function PreviewWidget() {
     }, []);
 
     if (metadata) {
+        const { data: { channelList } } = useContext(ChannelContext);
+
         return (
-            <p>test</p>
+            <div className="space-y-2">
+                <List list={channelList}><Frame /></List>
+            </div>
         )
     }
+}
+
+function Frame() {
+    const { item: { data: { stream: { source: { url } } } } } = useContext(ListContext);
+
+    if (!url) {
+        return;
+    }
+
+    return (
+        <iframe src={url} />
+    )
 }

35b005ca9d3958832b0d5a3ca480ca85546297ab
Ailakks <hola@ailakks.dev>
2024-04-16 19:26:36 +0200

    perf: remove key wrapper

--- src/wrapper/Logged.tsx
+++ src/wrapper/Logged.tsx
@@ -1,16 +1,11 @@
 import {useContext} from "react";
 import {AccountContext} from "./api/Account";
-import KeyWrapper from "./logged/Key";
 
 export default function LoggedWrapper({ children }) {
     const { data } = useContext(AccountContext);
 
     if (data) {
-        return (
-            <KeyWrapper>
-                {children}
-            </KeyWrapper>
-        )
+        return children;
     }
 
     return children;

a8cb897ed02af32166869ab027908ec6f2984f6a
Ailakks <hola@ailakks.dev>
2024-06-20 17:49:34 +0200

    feat: player controls

---
+++ src/components/player/Controls.tsx
@@ -0,0 +1,31 @@
+import {Tooltip} from "../Tooltip";
+
+export function PlayerControls() {
+    return (
+        <div>
+            <Tooltip text="Shuffle">
+                <button onClick={() => shuffle()}>
+                    <i className="fa-solid fa-shuffle" />
+                </button>
+            </Tooltip>
+            <Tooltip text="Previous">
+                <button onClick={() => handleBefore()}>
+                    <i className="fa-solid fa-backward-step" />
+                </button>
+            </Tooltip>
+            <button onClick={() => togglePlay()}>
+                <i className={isPlaying ? 'fa-solid fa-pause' : 'fa-solid fa-play'} />
+            </button>
+            <Tooltip text="Next">
+                <button onClick={() => handleNext()}>
+                    <i className="fa-solid fa-forward-step" />
+                </button>
+            </Tooltip>
+            <Tooltip text="Loop">
+                <button onClick={() => alterLoop()}>
+                    <i className={Object.values(LoopMode)[loop].icon} />
+                </button>
+            </Tooltip>
+        </div>
+    )
+}
--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -1,10 +1,12 @@
 import {Fragment} from "react";
 import {PlayerAudio} from "./Audio";
+import {PlayerControls} from "./Controls";
 
 export function Player({ children }) {
     return (
         <Fragment>
             <PlayerAudio />
+            <PlayerControls />
             {children}
         </Fragment>
     )

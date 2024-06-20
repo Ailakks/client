8183e41bb031710c86a5a3ab89c8332d07c63d00
Ailakks <hola@ailakks.dev>
2024-06-21 00:34:15 +0200

    perf: volume track as percent

--- src/components/player/controls/bar/Volume.tsx
+++ src/components/player/controls/bar/Volume.tsx
@@ -9,17 +9,17 @@ export function PlayerVolumeBar() {
     const [volume, setVolume] = useState(1);
 
     const handleRangeChange = (event) => {
-        setVolume(Math.pow(event.target.value, 2));
+        setVolume(Math.pow(event.target.value / 100, 2));
     };
 
     useEffect(() => {
-        player.current.volume = volume;
+        player.current.volume = volume / 100;
     }, [player, volume]);
 
     const VolumeStates = {
-        LOW: {value: 0.3, icon: 'fa-regular fa-volume-low'},
-        NORMAL: {value: 0.6, icon: 'fa-regular fa-volume'},
-        HIGH: {value: 1, icon: 'fa-regular fa-volume-high'},
+        LOW: { value: 30, icon: 'fa-regular fa-volume-low' },
+        NORMAL: { value: 60, icon: 'fa-regular fa-volume' },
+        HIGH: { value: 100, icon: 'fa-regular fa-volume-high' }
     };
 
     const toggleMute = () => {
@@ -29,18 +29,18 @@ export function PlayerVolumeBar() {
         }
 
         setPrevious(volume);
-        setVolume(0 ?? 0.75);
+        setVolume(0);
     };
 
     return (
         <div className="flex items-center space-x-4">
-            <button className={volume <= 0 ? 'fa-regular fa-volume-mute' : Object.values(VolumeStates).find((value) => volume <= value.value).icon} onClick={toggleMute} />
+            <button className={volume <= 0 ? 'fa-regular fa-volume-mute' : Object.values(VolumeStates).find((value) => volume * 100 <= value.value).icon} onClick={toggleMute} />
             <div className="grow">
                 <SlideBar
-                    value={Math.sqrt(volume)}
+                    value={Math.sqrt(volume) * 100}
                     min={0}
-                    max={1}
-                    step={0.01}
+                    max={100}
+                    step={1}
                     onChange={handleRangeChange}
                 />
             </div>

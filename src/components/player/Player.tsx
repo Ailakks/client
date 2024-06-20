e77c458ba4fd19c839254435687941a500531352
Ailakks <hola@ailakks.dev>
2024-06-21 00:09:04 +0200

    feat: player volume track

--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -2,6 +2,7 @@ import {PlayerAudio} from "./Audio";
 import {PlayerControls} from "./Controls";
 import {PlayerQueue} from "./Queue";
 import {PlayerTrackBar} from "./controls/bar/Track";
+import {PlayerVolumeBar} from "./controls/bar/Volume";
 
 export function Player({ children }) {
     return (
@@ -9,7 +10,14 @@ export function Player({ children }) {
             <PlayerAudio>
                 <div className="p-10 space-y-5">
                     <PlayerControls />
-                    <PlayerTrackBar />
+                    <div className="flex items-center space-x-5">
+                        <div className="grow">
+                            <PlayerTrackBar />
+                        </div>
+                        <div className="w-40">
+                            <PlayerVolumeBar />
+                        </div>
+                    </div>
                 </div>
             </PlayerAudio>
             {children}
--- src/components/player/controls/bar/Volume.tsx
+++ src/components/player/controls/bar/Volume.tsx
@@ -1,8 +1,9 @@
 import {useContext, useEffect, useState} from "react";
 import {SlideBar} from "../../ui/SlideBar";
+import {PlayerAudioContext} from "../../Audio";
 
 export function PlayerVolumeBar() {
-    const { audio } = useContext(AudioContext);
+    const { player } = useContext(PlayerAudioContext);
 
     const [previous, setPrevious] = useState(null);
     const [volume, setVolume] = useState(1);
@@ -12,8 +13,8 @@ export function PlayerVolumeBar() {
     };
 
     useEffect(() => {
-        audio.current.volume = volume;
-    }, [audio, volume]);
+        player.current.volume = volume;
+    }, [player, volume]);
 
     const VolumeStates = {
         LOW: {value: 0.3, icon: 'fa-regular fa-volume-low'},
@@ -32,15 +33,17 @@ export function PlayerVolumeBar() {
     };
 
     return (
-        <div>
+        <div className="flex items-center space-x-4">
             <button className={volume <= 0 ? 'fa-regular fa-volume-mute' : Object.values(VolumeStates).find((value) => volume <= value.value).icon} onClick={toggleMute} />
-            <SlideBar
-                value={Math.sqrt(volume)}
-                min={0}
-                max={1}
-                step={0.01}
-                onChange={handleRangeChange}
-            />
+            <div className="grow">
+                <SlideBar
+                    value={Math.sqrt(volume)}
+                    min={0}
+                    max={1}
+                    step={0.01}
+                    onChange={handleRangeChange}
+                />
+            </div>
         </div>
     );
 }

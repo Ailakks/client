e72260d80d29adbbaa56056ce158fb96467a3f2b
Ailakks <hola@ailakks.dev>
2024-06-21 00:39:32 +0200

    perf: player wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import { Player } from "./components/player/Player";
-import {TrackWrapper} from "./components/player/wrapper/Track";
-import {QualityWrapper} from "./components/player/wrapper/Quality";
-import {QueueWrapper} from "./components/player/wrapper/Queue";
-import {SourceWrapper} from "./components/player/wrapper/Source";
-import {AudioWrapper} from "./components/player/wrapper/Audio";
+import {Player} from "./components/player/Player";
 import {PlayerSource} from "./components/player/Source";
 
 export default function App() {
     return (
-        <TrackWrapper>
-            <QualityWrapper>
-                <QueueWrapper>
-                    <SourceWrapper>
-                        <AudioWrapper>
-                            <Player>
-                                <PlayerSource source="https://link.storjshare.io/s/jufww76lrzupipxnoqt7dtsry62a/111/sample.mp3?wrap=0" />
-                            </Player>
-                        </AudioWrapper>
-                    </SourceWrapper>
-                </QueueWrapper>
-            </QualityWrapper>
-        </TrackWrapper>
+        <Player>
+            <PlayerSource source="https://link.storjshare.io/s/jufww76lrzupipxnoqt7dtsry62a/111/sample.mp3?wrap=0" />
+        </Player>
     )
 }
--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -3,24 +3,39 @@ import {PlayerControls} from "./Controls";
 import {PlayerQueue} from "./Queue";
 import {PlayerTrackBar} from "./controls/bar/Track";
 import {PlayerVolumeBar} from "./controls/bar/Volume";
+import {QualityWrapper} from "./wrapper/Quality";
+import {QueueWrapper} from "./wrapper/Queue";
+import {SourceWrapper} from "./wrapper/Source";
+import {AudioWrapper} from "./wrapper/Audio";
+import {TrackWrapper} from "./wrapper/Track";
 
 export function Player({ children }) {
     return (
-        <PlayerQueue>
-            <PlayerAudio>
-                <div className="p-10 space-y-5">
-                    <PlayerControls />
-                    <div className="flex items-center space-x-5">
-                        <div className="grow">
-                            <PlayerTrackBar />
-                        </div>
-                        <div className="w-40">
-                            <PlayerVolumeBar />
-                        </div>
-                    </div>
-                </div>
-            </PlayerAudio>
-            {children}
-        </PlayerQueue>
+        <TrackWrapper>
+            <QualityWrapper>
+                <QueueWrapper>
+                    <SourceWrapper>
+                        <AudioWrapper>
+                            <PlayerQueue>
+                                <PlayerAudio>
+                                    <div className="p-10 space-y-5">
+                                        <PlayerControls />
+                                        <div className="flex items-center space-x-5">
+                                            <div className="grow">
+                                                <PlayerTrackBar />
+                                            </div>
+                                            <div className="w-40">
+                                                <PlayerVolumeBar />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </PlayerAudio>
+                                {children}
+                            </PlayerQueue>
+                        </AudioWrapper>
+                    </SourceWrapper>
+                </QueueWrapper>
+            </QualityWrapper>
+        </TrackWrapper>
     )
 }

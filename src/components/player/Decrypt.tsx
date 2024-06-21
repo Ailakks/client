630fc97880db01135ac9d59775288dc6d105f536
Ailakks <hola@ailakks.dev>
2024-06-21 22:01:22 +0200

    feat: player decrypt

---
+++ src/components/player/Decrypt.tsx
@@ -0,0 +1,47 @@
+import {createContext} from "react";
+
+export const PlayerDecryptContext = createContext();
+
+export default function PlayerDecrypt({ children }) {
+    const decrypt = (array) => {
+        const trackBuffer = Buffer.from(array, 'binary');
+
+        let chunk_size = 2048;
+        const blowFishKey = getBlowfishKey(id.toString());
+        let i = 0;
+        let position = 0;
+
+        const buffer = Buffer.alloc(trackBuffer.length);
+
+        while (position < trackBuffer.length) {
+            let chunk;
+            if (trackBuffer.length - position >= 2048) {
+                chunk_size = 2048;
+            } else {
+                chunk_size = trackBuffer.length - position;
+            }
+            chunk = Buffer.alloc(chunk_size);
+
+            trackBuffer.copy(chunk, 0, position, position + chunk_size);
+
+            let chunkString;
+            if (i % 3 > 0 || chunk_size < 2048) {
+                chunkString = chunk.toString('binary');
+            } else {
+                chunkString = decryptChunk(chunk, blowFishKey);
+            }
+
+            Buffer.from(chunkString, 'binary').copy(buffer, position);
+            position += chunk_size;
+            i++;
+        }
+
+        return buffer;
+    };
+
+    return (
+        <PlayerDecryptContext.Provider value={{ decrypt }}>
+            {children}
+        </PlayerDecryptContext.Provider>
+    )
+}

53d854c16f06a6d64a82d98ceed6b3c08e9c0a09
Ailakks <hola@ailakks.dev>
2024-06-21 22:05:38 +0200

    feat: player source blob

--- src/components/item/TrackPlayer.tsx
+++ src/components/item/TrackPlayer.tsx
@@ -2,7 +2,9 @@ import {useContext} from "react";
 import Query from "../query/Query";
 import {AxiosAppContext} from "../wrapper/api/App";
 import {PlayerContext} from "../wrapper/player/Player";
-import PlayerDecrypt from "../player/Decrypt";
+import PlayerDecrypt, {PlayerDecryptContext} from "../player/Decrypt";
+import {Player} from "../player/Player";
+import {PlayerSource} from "../player/Source";
 
 export default function TrackPlayer() {
     const { track } = useContext(PlayerContext);
@@ -24,6 +26,7 @@ export default function TrackPlayer() {
 
 function Body() {
     const { useClient } = useContext(AxiosAppContext);
+    const { get } = useContext(PlayerDecryptContext);
 
     const { track: { id } } = useContext(PlayerContext);
 
@@ -32,10 +35,18 @@ function Body() {
     return (
         <Query request={request}>
             {({ media: { sources: { [0]: { url } } } }) => {
+                const blob = await get(url);
 
+                if (!blob) {
+                    return (
+                        <p>loading</p>
+                    )
+                }
 
                 return (
-                    <p>{url}</p>
+                    <Player>
+                        <PlayerSource source={blob} />
+                    </Player>
                 )
             }}
         </Query>

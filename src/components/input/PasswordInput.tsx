9d5ddd378f749d3276afe9432f88a96ec06b90f1
Ailakks <hola@ailakks.dev>
2024-06-22 01:40:26 +0200

    feat: password input

---
+++ src/components/input/PasswordInput.tsx
@@ -0,0 +1,19 @@
+import {useContext, useState} from "react";
+import {clsx} from "clsx";
+import {LanguageContext} from "../wrapper/lang/Language";
+import {Input} from "../query/Input";
+
+export function PasswordInput() {
+    const { translate } = useContext(LanguageContext);
+
+    const [visible, setVisible] = useState(false);
+
+    return (
+        <div className="flex items-center relative">
+            <Input name="password" type={visible ? 'text' : 'password'} className="main w-full" placeholder={translate("auth.password.label")} required />
+            <span onClick={() => setVisible((prevState) => !prevState)} className="absolute cursor-pointer right-2 p-2">
+                <i className={clsx(visible ? 'fa-regular fa-eye-slash' : 'fa-regular fa-eye', 'text-white')} />
+            </span>
+        </div>
+    )
+}

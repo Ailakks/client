a1918f3c5099ef99cd17dc1a4037db6f14031ae4
Ailakks <hola@ailakks.dev>
2024-06-21 18:06:05 +0200

    feat: playlist item

--- src/pages/Playlist.tsx
+++ src/pages/Playlist.tsx
@@ -2,6 +2,7 @@ import {useParams} from "react-router-dom";
 import {useContext} from "react";
 import {AxiosContext} from "../components/wrapper/Axios";
 import Query from "../components/query/Query";
+import List, {ListContext} from "../components/list/List";
 
 export default function Playlist() {
     const { useClient } = useContext(AxiosContext);
@@ -12,11 +13,20 @@ export default function Playlist() {
 
     return (
         <Query request={request}>
-            {(response, refetch) => {
+            {({ tracks }, refetch) => {
                 return (
-                    <p>{JSON.stringify(response)}</p>
+                    <List list={tracks}><Item /></List>
                 )
             }}
         </Query>
+    )
+}
+
+
+function Item() {
+    const { item: { track: { data: { name } } } } = useContext(ListContext);
+
+    return (
+        <p>{name}</p>
     )
 }

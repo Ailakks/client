ebf0619ffd959d855c812f0edd74b1484f8b6d4a
Ailakks <hola@ailakks.dev>
2024-06-21 17:48:06 +0200

    feat: playlist request

--- src/pages/Playlist.tsx
+++ src/pages/Playlist.tsx
@@ -1,5 +1,11 @@
 import {useParams} from "react-router-dom";
+import {useContext} from "react";
+import {AxiosContext} from "../components/wrapper/Axios";
 
 export default function Playlist() {
+    const { useClient } = useContext(AxiosContext);
+
     const { id } = useParams();
+
+    const request = useClient({ url: `playlist/${id}` });
 }

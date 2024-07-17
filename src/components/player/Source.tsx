c2eb079c1ed8302800bebdc1ae0695916ab4fd0a
Ailakks <hola@ailakks.dev>
2024-07-18 01:43:35 +0200

    perf: remove blob audio source

--- src/components/player/Source.tsx
+++ src/components/player/Source.tsx
@@ -6,8 +6,6 @@ export function PlayerSource({ source }) {
     const { setSource } = useContext(SourceContext);
 
     useEffect(() => {
-        axios.get(source, { responseType: "arraybuffer" }).then(({ data }) => {
-            setSource(URL.createObjectURL(new Blob([data], { type: 'audio/mp3' })));
-        });
+        setSource(source)
     }, [source]);
 }

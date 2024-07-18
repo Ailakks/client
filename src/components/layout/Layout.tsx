a7f6dc88e18ce328128860d4f6d4abf3e1fbbb07
Ailakks <hola@ailakks.dev>
2024-07-18 18:23:37 +0200

    perf: layout style

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({ children }) {
 
     return (
         <LayoutContext.Provider value={{side, setSide}}>
-            <div className="h-full flex flex-col mx-auto max-w-screen-2xl">
+            <div className="h-full flex flex-col mx-auto max-w-screen-2xl mx-10">
                 <div className="flex h-20 shrink-0">
                     <div className="flex w-64 shrink-0 items-center">
                         <Head />

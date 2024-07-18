e90ae0fdc6957eb123b562d648eb49e69ea56655
Ailakks <hola@ailakks.dev>
2024-07-18 19:07:36 +0200

    feat: track artist list

--- src/components/player/Data.tsx
+++ src/components/player/Data.tsx
@@ -1,13 +1,32 @@
 import {useContext} from "react";
 import {PlayerContext} from "../wrapper/player/Player";
+import List, {ListContext} from "../list/List";
 
 export function PlayerData() {
-    const { track: { name, album: { image: { url } } } } = useContext(PlayerContext);
+    const { track: { name, album: { image: { url } }, artists } } = useContext(PlayerContext);
 
     return (
         <div className="flex space-x-2 items-center">
             <img className="h-20 rounded-md" alt={name} src={url} />
-            <p>{name}</p>
+            <div>
+                <p>{name}</p>
+                <p>
+                    <List list={artists}>
+                        <Artist/>
+                    </List>
+                </p>
+            </div>
         </div>
+    )
+}
+
+function Artist() {
+    const { list, key, item: { id, name } } = useContext(ListContext);
+
+    return (
+        <span>
+            <a href={`/artist/${id}`}>{name}</a>
+            {key < (list.length - 1) && <span>, </span>}
+        </span>
     )
 }

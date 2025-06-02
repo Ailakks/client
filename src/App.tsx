23bf0fd1b68bf7d8b913b4aa28cd1837a4dc2f94
Javier Cortés Pérez <148952130+javiertch@users.noreply.github.com>
2025-06-02 22:43:59 +0000

    feat: main global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes, useLocation } from 'react-router-dom';
 import OAuthLogin from './pages/oauth-login';
 
+import "@repo/ui/styles/globals.css";
+
 export default function App() {
   const location = useLocation();
 

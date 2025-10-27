e1dbe418ca247cd6b6bddbbe1272e02fa97f0af9
Ailakks <hola@ailakks.dev>
2025-10-27 19:53:13 +0100

    feat: login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,98 @@
-import "./styles/globals.css";
+import "./index.css";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
+    Field,
+    FieldDescription,
+    FieldError,
+    FieldGroup,
+    FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Controller, useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
 
 export function App() {
-  return (
-    <p className="text-red-500">test</p>
-  );
+    const formSchema = z.object({
+        login: z
+            .email(),
+        password: z
+            .string()
+            .min(20)
+            .max(100),
+    });
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            login: "",
+            password: "",
+        },
+    });
+
+    function onSubmit(data: z.infer<typeof formSchema>) {
+    };
+
+    return (
+        <Card className="w-full sm:max-w-md">
+            <CardHeader>
+                <CardTitle>Iniciar sesión</CardTitle>
+                <CardDescription>Añade una cuenta para empezar.</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form id="login" onSubmit={form.handleSubmit(onSubmit)}>
+                    <FieldGroup>
+                        <Controller
+                            name="login"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+                                <Field data-invalid={fieldState.invalid}>
+                                    <FieldLabel>Correo electrónico</FieldLabel>
+                                    <Input
+                                        {...field}
+                                        aria-invalid={fieldState.invalid}
+                                        placeholder="email@example.com"
+                                        type="email"
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            )}
+                        />
+                        <Controller
+                            name="password"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+                                <Field data-invalid={fieldState.invalid}>
+                                    <FieldLabel>Contraseña</FieldLabel>
+                                    <Input
+                                        {...field}
+                                        aria-invalid={fieldState.invalid}
+                                        type="password"
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            )}
+                        />
+                    </FieldGroup>
+                </form>
+            </CardContent>
+            <CardFooter>
+                <Field orientation="horizontal">
+                    <Button type="submit" className="cursor-pointer">Iniciar sesión</Button>
+                </Field>
+            </CardFooter>
+        </Card>
+    );
 }

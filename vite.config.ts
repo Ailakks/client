5ac4371d72728037c0cada31bcfa4e1ae9fcd584
Ailakks <hola@ailakks.dev>
2025-10-27 20:14:34 +0100

    refactor: move shadcn components

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./"),
-      "lib": path.resolve(__dirname, "lib"),
+    server: {
+        port: 4000
     },
-  },
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })

ec7f854087b2d369c3f3ed39ce9da6a0b97f76e1
Javier Cortés Pérez <hola@cortmnzz.com>
2025-10-28 13:45:19 +0100

    feat: channel roles

--- src/lib/roles.tsx
+++ src/lib/roles.tsx
@@ -7,3 +7,9 @@ export function rolesMask(mask: string[], overwrites: { allow: string, deny: str
     return (acc & ~deny) | allow
   }, base)
 }
+
+export function channelApplyingRoles(member: { roles: string[] }, roles: [{ id: string, name: string }], permission_overwrites: [{ id: string }]): string[] {
+  const everyone = roles.find((item: { name: string }) => item.name == "@everyone");
+
+  return permission_overwrites.map((item: { id: string }) => item.id).filter(item => [everyone?.id, member.roles].includes(item));
+}

93b40dcdf2722980017cbf50a48deb5180571312
Javier Cortés Pérez <hola@cortmnzz.com>
2025-11-05 20:28:44 +0100

    refactor: gif picker style

--- src/pages/app/chat/gif-picker.tsx
+++ src/pages/app/chat/gif-picker.tsx
@@ -1,8 +1,7 @@
 import { StickersListTransform } from "@/api/transform/emoji/stickers.transform";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
-import { Card, CardHeader } from "@/components/ui/card";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useAxiosClient } from "@/lib/axios";
+import { Fragment } from "react/jsx-runtime";
 
 export function GifPicker() {
     const [{ data, loading }] = useAxiosClient<StickersListTransform>({ url: `gifs/trending`, params: { provider: 'tenor', media_format: 'webm' } }, { useCache: true })
@@ -12,25 +11,16 @@ export function GifPicker() {
     }
 
     return (
-        <Card>
-            <Tabs defaultValue="general" orientation="vertical">
-                <CardHeader>
-                    <TabsList className="flex-col w-full *:w-full space-y-2">
-                        <TabsTrigger value="gif">GIF</TabsTrigger>
-                        <TabsTrigger value="stickers">Stickers</TabsTrigger>
-                        <TabsTrigger value="emojis">Rmojis</TabsTrigger>
-                    </TabsList>
-                </CardHeader>
-                <TabsContent value="gif">
-                    {data.categories.map((item, key) => {
-                        return (
-                            <AspectRatio key={key}>
-                                <img src={item.src} alt={item.name} />
-                            </AspectRatio>
-                        )
-                    })}
-                </TabsContent>
-            </Tabs>
-        </Card>
+        <Fragment>
+            {data.categories.map((item, key) => {
+                return (
+                    <AspectRatio key={key} ratio={16 / 9} className="relative rounded-sm overflow-hidden">
+                        <video src={item.src} className="h-full w-full object-cover" />
+                        <div className="absolute inset-0 bg-black/80" />
+                        <p className="absolute inset-0 flex items-center justify-center text-white">{item.name}</p>
+                    </AspectRatio>
+                )
+            })}
+        </Fragment>
     );
 }
--- src/pages/app/chat/image-picker.tsx
+++ src/pages/app/chat/image-picker.tsx
@@ -4,16 +4,16 @@ import { GifPicker } from "./gif-picker";
 
 export function ImagePicker() {
     return (
-        <Card>
-            <Tabs defaultValue="gif" orientation="vertical">
+        <Card className="overflow-hidden">
+            <Tabs defaultValue="gif" className="flex h-100">
                 <CardHeader>
-                    <TabsList className="flex-col w-full *:w-full space-y-2">
+                    <TabsList className="w-full">
                         <TabsTrigger value="gif">GIF</TabsTrigger>
                         <TabsTrigger value="stickers">Stickers</TabsTrigger>
                         <TabsTrigger value="emojis">Emojis</TabsTrigger>
                     </TabsList>
                 </CardHeader>
-                <TabsContent value="gif">
+                <TabsContent value="gif" className="p-2 grid grid-cols-2 gap-2 overflow-auto">
                     <GifPicker />
                 </TabsContent>
             </Tabs>

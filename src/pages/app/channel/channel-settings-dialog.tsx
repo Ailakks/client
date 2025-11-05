4a2fb09cfc65fe44f9a6fcf06ca13b4b33584387
Javier Cortés Pérez <hola@cortmnzz.com>
2025-11-05 01:09:26 +0100

    feat: channel settings dialog

--- src/pages/app/channel/channel-settings-dialog.tsx
+++ src/pages/app/channel/channel-settings-dialog.tsx
@@ -2,6 +2,8 @@ import type { ChannelTransform } from "@/api/transform/channel.transform";
 import { DialogContent, DialogTitle } from "@radix-ui/react-dialog";
 import { DialogHeader } from "@/components/ui/dialog";
 import { Sidebar, SidebarContent, SidebarProvider } from "@/components/ui/sidebar";
+import { Tabs, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import { TabsList } from "@radix-ui/react-tabs";
 
 export function ChannelSettingsDialog({ item }: { item: ChannelTransform }) {
     return (
@@ -9,6 +11,33 @@ export function ChannelSettingsDialog({ item }: { item: ChannelTransform }) {
             <DialogHeader>
                 <DialogTitle>Vista general</DialogTitle>
             </DialogHeader>
+            <Tabs defaultValue="general">
+                <div className="flex">
+                    <SidebarProvider className="w-fit">
+                        <Sidebar>
+                            <SidebarContent>
+                                <p>#{item.name}</p>
+                                <TabsList>
+                                    <TabsTrigger value="general">general</TabsTrigger>
+                                    <TabsTrigger value="roles">roles</TabsTrigger>
+                                </TabsList>
+                            </SidebarContent>
+                        </Sidebar>
+                    </SidebarProvider>
+                    <div>
+                        <TabsList>
+                            <TabsTrigger value="general">general</TabsTrigger>
+                            <TabsTrigger value="roles">roles</TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="general">
+                            <p>general</p>
+                        </TabsContent>
+                        <TabsContent value="roles">
+                            <p>roles</p>
+                        </TabsContent>
+                    </div>
+                </div>
+            </Tabs>
         </DialogContent>
     );
 }
